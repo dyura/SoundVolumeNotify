45dfb4b4aa80d7503b52187b51dbf0b8ef6ff36f
yury <yury.dubinsky@gmail.com>
2019-09-28 12:03:06 -0400

    Changed BootReceiver to start without activity to satisfy Android 10 Change the application name to "Sound Volume Notifier"

--- app/src/main/java/soundvolume/my/soundvolumenotify/BootReceiver.java
+++ app/src/main/java/soundvolume/my/soundvolumenotify/BootReceiver.java
@@ -1,17 +1,28 @@
 package soundvolume.my.soundvolumenotify;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import static soundvolume.my.soundvolumenotify.AlarmManagerBroadcastReceiver.ONE_TIME;
+
 public class BootReceiver extends BroadcastReceiver {
+    public static final int NOTIFICATION_ID = 1;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent i = new Intent(context, SoundVolumeActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(i);
+            NotificationFunc sn = new NotificationFunc() ;
+            sn.sendNotification(context,NOTIFICATION_ID);
+            AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+            Intent ABRintent = new Intent(context, AlarmManagerBroadcastReceiver.class);
+            ABRintent.putExtra("name","main".toString());
+            ABRintent.putExtra(ONE_TIME, Boolean.FALSE);
+            PendingIntent pi = PendingIntent.getBroadcast(context, 0, ABRintent, 0);
+            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 , pi);
+
         }
     }
 }
